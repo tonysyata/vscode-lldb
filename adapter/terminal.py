e2866c1b4999a94d0c121205ed1b2748e1663d6b
Vadim Chugunov <vadimcn@gmail.com>
2018-09-11 16:52:38 -0700

    Misc

--- adapter/terminal.py
+++ adapter/terminal.py
@@ -21,9 +21,9 @@ def create(spawn_terminal=None):
     ls.bind(('127.0.0.1', 0))
     ls.listen(1)
     addr, port = ls.getsockname()
-    # terminal.sh opens a TCP connection, sends output of `tty`, 
+    # terminal.sh opens a TCP connection, sends output of `tty`,
     # waits till the socket gets closed from our end
-    args = [os.path.join(os.path.dirname(__file__), 'terminal.sh'), str(port)]
+    args = ['/bin/bash', '-c', 'exec 3<>/dev/tcp/127.0.0.1/%d; tty >&3; clear; read <&3' % port]
     if spawn_terminal is not None:
         spawn_terminal(args)
     else:
